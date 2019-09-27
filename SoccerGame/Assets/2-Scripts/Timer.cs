11686c9a7daabce0584634eaa00b79f1888bfa8f
Juan Martin Garcia <juanmar-p@hotmail.com>
2019-09-26 20:27:59 -0500

    Cronometro del partido

---
+++ SoccerGame/Assets/2-Scripts/Timer.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+
+    public Text tiempoText;
+    public float tiempo = 59f;
+
+    public void Start()
+    {
+
+        tiempoText.text = "00:" + tiempo.ToString("f0");
+
+    }
+
+
+    public void Update()
+    {
+
+        if (tiempo >= 0)
+        {
+
+            tiempo -= Time.deltaTime;
+
+            tiempoText.text = "00:" + tiempo.ToString("f0");
+            if (tiempo < 9)
+            {
+                tiempoText.text = "00:" + "0" + tiempo.ToString("f0") + "";
+            }
+            else if (tiempo == 0)
+            {
+                tiempoText.text = "00:00";
+
+            }
+
+        }
+
+    }
+
+
+}
