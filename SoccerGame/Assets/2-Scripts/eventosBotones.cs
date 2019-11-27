0f8a9d6459612de3769c290f38fc8f2dfb49ff06
DouglasLopezMena <douglasmena2016@gmail.com>
2019-11-27 16:44:03 -0500

    Menu realizado

---
+++ SoccerGame/Assets/2-Scripts/eventosBotones.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class eventosBotones : MonoBehaviour
+{
+  
+    public void botonJugarOnline()
+    {
+        SceneManager.LoadScene("ClientLogin");
+
+    }
+
+    public void botonSalir()
+    {
+        Application.Quit();
+
+    }
+
+
+}
